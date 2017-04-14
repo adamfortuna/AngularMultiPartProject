6eb9b37aac541d21274a59fee4b1d4140e98136c
Jon Friskics <jonfriskics@gmail.com>
2017-04-13 22:26:47 -0400

    initial state for Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
